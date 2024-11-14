24d02661dac6a33fc54ce27acfdcbdf04399bf29
Elvis Kuria <levonjoroge>
2024-11-14 15:25:09 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
-import ContactUs from './pages/Contact us';
-import Form  from './pages/Form';
-import { BrowserRouter as Router,Routes,Route, Form } from 'react-router-dom';
+import ContactUs from './pages/Contact us'; 
+import Form from './pages/Form';
+
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    
+  
     <Router>
       <Routes>
-        <Route path='/' element={<Landing/>}></Route>
-        <Route index element={<Home/>}></Route>
-         <Route path='contact us' element={<ContactUs/>}></Route>
-         <Route path='Add Recipe' element={<Form/>}></Route>
-      </Routes> 
+        <Route path='/' element={<Home/>} />
+        
+        <Route path='/contact-us' element={<ContactUs />} > </Route>
+        <Route path='/add-recipe' element={<Form />}></Route> 
+      </Routes>
     </Router>
-  
   );
 }
 
